1c8514d2f081be6d47892a236f1f23a1af964350
Arevyn24 <s_shurie@yahoo.com>
2017-07-06 17:00:46 -0400

    added some startup functions

---
+++ assets/js/pomadoro.js
@@ -0,0 +1,24 @@
+$(function(){
+
+	$.getJSON('assets/default.json', function(data) {
+		for(var i in data) {
+			$('input[name="'+ i +'"]').val(data[i]);
+		}
+	});
+
+  $('#start').on('click', function(e){
+    e.preventDefault();
+    console.log("Beginning timer");
+  });
+
+  $("pause").on('click', function(e){
+  	e.preventDefault();
+  });
+
+    $("reset").on('click', function(e){
+  	e.preventDefault();
+  	for(var i in data) {
+			$('input[name="'+ i +'"]').val(data[i]);
+		}
+  });
+});
