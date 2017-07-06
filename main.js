d3a99f96cf9d8ef072f9b13116de58d3f42fe2ac
Gary A. Newsome <garyanewsome@gmail.com>
2017-07-06 13:11:33 -0400

    resize and gradient

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 480, height: 960})
+  win = new BrowserWindow({width: 480, height: 560})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
