48b7a734aaa389d0468c46772c946505437cee38
Gary A. Newsome <garyanewsome@gmail.com>
2017-07-11 15:20:16 -0400

    Working countdown timer and notification

--- assets/js/pomadoro.js
+++ assets/js/pomadoro.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 $(function(){
 
 	$.getJSON('assets/default.json', function(data) {
@@ -26,23 +28,41 @@ $(function(){
   });
 });
 
+function notify() {
+  var notification = new Notification("Timer Stopped")
+  notification
+}
+
+function stopTimer(countdown) {
+  clearInterval(countdown)
+}
+
+function timer(time) {
+  var duration = moment.duration(time)
+  var interval = 1
+  var countdown = setInterval(function () {
+    duration = moment.duration(duration.asSeconds() - interval, 'seconds');
+    if(duration._milliseconds > 0) {
+      var time = moment(duration._milliseconds).format('mm:ss')
+      $('#current-time').text(time)
+      // $('#current-time').text(Math.round(duration.minutes()) + ':' + Math.round(duration.seconds()))
+    }
+    if(duration._milliseconds === 0) {
+      $('#current-time').text('00:00')
+      notify()
+    }
+    if(duration._milliseconds < 0) {        stopTimer(countdown)
+    }
+  }, 1000)
+  countdown
+}
 
 function runTimer(pomodoro) {
-  const moment = require('moment');
   var startTime = moment().valueOf();
   var pomoForRealDo = pomodoro * 60 * 1000
-  console.log(pomoForRealDo)
   var endTime = startTime + pomoForRealDo
-  console.log(endTime)
   var diff = moment(pomoForRealDo).format("mm:ss")
-  console.log(diff)
   $("#current-time").text(diff);
-  // var startTimeFormatted = startTime.format("h:mm:ss")
-  // var endTime = startTime.add(pomodoro, "minutes");
-  // var endTimeFormatted = endTime.format("h:mm:ss")
-
-
-  /** Code gets iffy here **/
-  //var moment = require("moment");
+  timer(pomoForRealDo)
 }
 
