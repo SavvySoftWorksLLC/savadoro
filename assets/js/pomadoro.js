e91be1e9a40686a910f99849e0924708b51b0970
Arevyn24 <s_shurie@yahoo.com>
2017-07-13 13:42:23 -0400

    wip

--- assets/js/pomadoro.js
+++ assets/js/pomadoro.js
@@ -3,25 +3,29 @@ const moment = require('moment');
 $(function(){
 
 	$.getJSON('assets/default.json', function(data) {
+
 		for(var i in data) {
 			$('input[name="'+ i +'"]').val(data[i]);
 		}
 		$("#current-time").text('0:00');
-		$("#current-session").text('None');
+		$("#current-session").text('0');
+    window.onchange=getSessionInfo;
 	});
+
+
 
   $('#start').on('click', function(e){
     e.preventDefault();
-    getSessions();
+    savadoro();
     var pomodoro = $("#pomodoro").val();
     runTimer(pomodoro);
   });
 
-  $("pause").on('click', function(e){
+  $("#pause").on('click', function(e){
   	e.preventDefault();
   });
 
-    $("reset").on('click', function(e){
+    $("#reset").on('click', function(e){
   	e.preventDefault();
   	for(var i in data) {
 			$('input[name="'+ i +'"]').val(data[i]);
@@ -29,22 +33,35 @@ $(function(){
   });
 });
 
-
-
-
-function getSessions() {
+function getSessionInfo() {
   var pomodoro = $('#pomodoro').val();
   var shortBreak = $('#short-break').val();
   var longBreak = $('#long-break').val();
   var sessionsNum = $('#sessions').val();
-  var sessions = [['pomodor', 10], ['short break', 3], ['long break', 15], ['sessions', 4]];
-  for (var i=0; i < sessions[3][1]; i++){
-    console.log('butt')
-  }
-  for (var ii=0; ii < sessions[3][1]; ii++){
-    console.log(sessions[0][0]);
-    console.log(sessions[1][0]);
-  }
+  var sessions = {'pomodoro': pomodoro, 'short break': shortBreak, 'long break': longBreak,
+   'sessions': sessionsNum};
+  return sessions;
+}
+
+function savadoro() {
+  var sessionsInfo = getSessionInfo();
+  var sessionLength = sessionsInfo['sessionsNum'];
+  var stage = 1;
+  var currentSession = 1;
+  runTimer()
+
+
+}
+
+function runPomodoro(pomodoroTime) {
+  runTimer(pomodoroTime);
+}
+
+function changeSession(){
+  var currentSession = $("#current-session").val();
+  currentSession = currentSession + 1;
+  var totalSessions = $('#sessions').val();
+  $("#current-session").text(currentSession);
 }
 
 function notify() {
@@ -61,23 +78,23 @@ function timer(time) {
   var interval = 1
   var countdown = setInterval(function () {
     duration = moment.duration(duration.asSeconds() - interval, 'seconds');
-    if(duration._milliseconds > 0) {
+    if(duration._milliseconds > -1) {
       var time = moment(duration._milliseconds).format('mm:ss')
       $('#current-time').text(time)
       // $('#current-time').text(Math.round(duration.minutes()) + ':' + Math.round(duration.seconds()))
     }
     if(duration._milliseconds === 0) {
-      $('#current-time').text('00:00')
       notify()
-    }
-    if(duration._milliseconds < 0) {
       stopTimer(countdown)
+      $('#current-time').text('00:00')
+
     }
   }, 1000)
   countdown
 }
 
 function runTimer(pomodoro) {
+  changeSession();
   var startTime = moment().valueOf();
   var pomoForRealDo = pomodoro * 60 * 1000
   var endTime = startTime + pomoForRealDo
